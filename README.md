582da999a558ff044cb069edabcb9610272c226e
eliezerraj <eliezerraj@DDHDZ8JH3.conductor.tecnologia>
2024-10-05 00:32:14 -0300

    add order table

--- README.md
+++ README.md
@@ -4,4 +4,7 @@ POC for test purposes.
 
 # Run
 
-kubectl apply -f job.yaml
+1. Add the changes in changelog.yaml
+2. Create a docker image (make)
+3. Upload image to ECR
+4. kubectl apply -f job.yaml
---
+++ migrations/12-create-table-order.sql
@@ -0,0 +1,20 @@
+-- public.order definition
+-- Drop table
+-- DROP TABLE public.order;
+
+CREATE TABLE public.order (
+	id serial4 NOT NULL,
+	order_id varchar(200) NULL,
+	person_id varchar(200) NULL,
+	status varchar(200) NULL,
+	currency varchar(10) NULL,
+	amount float8 NULL,
+	create_at timestamptz NULL,
+	update_at timestamptz NULL,
+	tenant_id varchar(200) null,
+	CONSTRAINT order_pkey PRIMARY KEY (id)
+);
+
+CREATE INDEX order_idx ON public.order USING btree (order_id);
+
+CREATE INDEX order_person_idx ON public.order USING btree (person_id);
