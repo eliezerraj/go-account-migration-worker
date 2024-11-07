9259b039da4e00cfd9bffd664ea7335230909917
eliezerraj <eliezerraj@DDHDZ8JH3.conductor.tecnologia>
2024-11-06 22:50:57 -0300

    add card

--- migrations/05-create-table-card.sql
+++ migrations/05-create-table-card.sql
@@ -9,6 +9,7 @@ CREATE TABLE public.card (
 	fk_account_id int4 NULL,
 	card_number varchar(200) NULL,
 	card_type varchar(200) NULL,
+	card_model varchar(200) NULL,
 	card_pin varchar(200) NULL,
 	status varchar(200) NULL,
 	expire_at timestamptz NULL,
@@ -17,8 +18,8 @@ CREATE TABLE public.card (
 	tenant_id varchar(200) NULL,
 	CONSTRAINT card_pkey PRIMARY KEY (id)
 );
-
 CREATE INDEX card_idx ON public.card USING btree (card_number);
 
 -- public.card foreign keys
+
 ALTER TABLE public.card ADD CONSTRAINT card_fk_account_id_fkey FOREIGN KEY (fk_account_id) REFERENCES public.account(id);
