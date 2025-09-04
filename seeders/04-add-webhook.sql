2ddea5c04f2753238c93afb0d366a0bf5d4000a6
eliezerraj <eliezerraj@gmail.com>
2025-09-04 20:53:22 -0300

    add:seeder webhook

--- seeders/04-add-webhook.sql
+++ seeders/04-add-webhook.sql
@@ -1,7 +1,15 @@
 INSERT INTO public.webhook_config
 (id, receiver, "type", host, url, "method", created_at, updated_at)
-VALUES(nextval('webhook_config_id_seq'::regclass), 'ACCOUNT:ACC-1', 'TOPIC:PIX', 'https://teste.com', '/teste', 'POST', NOW(), NULL);
+VALUES(nextval('webhook_config_id_seq'::regclass), 'ACCOUNT:ACC-1', 'TOPIC:PIX', 'https://teste2.com', '/teste', 'POST', NOW(), NULL);
 
 INSERT INTO public.webhook_config
 (id, receiver, "type", host, url, "method", created_at, updated_at)
-VALUES(nextval('webhook_config_id_seq'::regclass), 'ACCOUNT:ACC-2', 'TOPIC:PIX', 'https://teste.com', '/teste', 'GET', NOW(), NULL);
+VALUES(nextval('webhook_config_id_seq'::regclass), 'ACCOUNT:ACC-2', 'TOPIC:PIX', 'https://teste2.com', '/teste', 'GET', NOW(), NULL);
+
+INSERT INTO public.webhook_config
+(id, receiver, "type", host, url, "method", created_at, updated_at)
+VALUES(nextval('webhook_config_id_seq'::regclass), 'ACCOUNT:ACC-3', 'TOPIC:PIX', 'https://teste2.com', '/teste', 'GET', NOW(), NULL);
+
+INSERT INTO public.webhook_config
+(id, receiver, "type", host, url, "method", created_at, updated_at)
+VALUES(nextval('webhook_config_id_seq'::regclass), 'ACCOUNT:ACC-4', 'TOPIC:PIX', 'https://teste2.com', '/teste', 'GET', NOW(), NULL);
